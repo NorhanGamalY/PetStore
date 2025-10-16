a29af38174f2a0499c0927072dab6b1544d8fa1a
Norhan Gamal <norhan25gamal@gmail.com>
2025-10-16 23:33:04 +0300

    Navbar and routes are done

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,4 @@
 import { bootstrapApplication } from '@angular/platform-browser';
-import 'bootstrap/dist/js/bootstrap.bundle.min.js';
 import { appConfig } from './app/app.config';
 import { App } from './app/app';
 
