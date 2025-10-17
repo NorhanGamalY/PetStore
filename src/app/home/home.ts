199a0a2f22d5b3b9dd393cb53e251c0af7d1c38c
Marcy <marsy2126@gmail.com>
2025-10-17 21:30:47 +0300

    Add responsive footer component

--- src/app/home/home.ts
+++ src/app/home/home.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
 import { Hero } from './hero/hero';
 import { CategoriesComponent } from './categories/categories';
+import { Footer } from './footer/footer';
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [Hero,  CategoriesComponent],
+  imports: [Hero,  CategoriesComponent,Footer],
 templateUrl: './home.html',
 })
 export class Home {}
