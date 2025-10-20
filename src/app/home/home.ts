d643bda5e98e6cc3994e23811e9741dc2354a1b0
Marcy <marsy2126@gmail.com>
2025-10-20 20:55:31 +0300

    Add Shop By Pets component

--- src/app/home/home.ts
+++ src/app/home/home.ts
@@ -3,11 +3,12 @@ import { Hero } from './hero/hero';
 import { CategoriesComponent } from './categories/categories';
 import { Footer } from './footer/footer';
 import { NewsBlog } from './news-blog/news-blog';
+import { ShopByPetsComponent } from "./shop-by-pets/shop-by-pets";
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [Hero,  CategoriesComponent,Footer,NewsBlog],
+  imports: [Hero, CategoriesComponent, Footer, NewsBlog, ShopByPetsComponent],
 templateUrl: './home.html',
 })
 export class Home {}
