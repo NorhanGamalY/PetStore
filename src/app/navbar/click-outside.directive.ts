c4742c1c31e2f57e585f97e0c21eebb87f67095f
Norhan Gamal <norhan25gamal@gmail.com>
2025-11-01 23:14:51 +0200

    Fix type script error

--- src/app/navbar/click-outside.directive.ts
+++ src/app/navbar/click-outside.directive.ts
@@ -9,9 +9,9 @@ export class ClickOutsideDirective {
 
   constructor(private elementRef: ElementRef) {}
 
-  @HostListener('document:click', ['$event.target'])
-  public onClick(target: HTMLElement): void {
-    const clickedInside = this.elementRef.nativeElement.contains(target);
+ @HostListener('document:click', ['$event'])
+  public onClick(event: Event): void {
+    const clickedInside = this.elementRef.nativeElement.contains(event.target);
     if (!clickedInside) {
       this.clickOutside.emit();
     }
